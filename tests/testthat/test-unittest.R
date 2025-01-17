5fb7363fea2935e2b82a1adec8f281456c96fbc1
amyheather <a.heather2@exeter.ac.uk>
2025-01-17 10:51:59 +0000

    test(unittest): use process_replications() rather than raw monitoring

--- tests/testthat/test-unittest.R
+++ tests/testthat/test-unittest.R
@@ -8,46 +8,17 @@ test_that("the same seed returns the same result", {
   # Run model twice using same run number (which will set the seed)
   env1 <- model(run_number = 0L, param = defaults())
   env2 <- model(run_number = 0L, param = defaults())
-
-  # Compare output results
-  expect_identical(
-    env1 %>% simmer::get_mon_arrivals(),
-    env2 %>% simmer::get_mon_arrivals()
-  )
-  expect_identical(
-    env1 %>% simmer::get_mon_resources(),
-    env2 %>% simmer::get_mon_resources()
-  )
+  expect_identical(process_replications(env1), process_replications(env2))
 
   # Conversely, if run with different run number, expect different
   env1 <- model(run_number = 0L, param = defaults())
   env2 <- model(run_number = 1L, param = defaults())
-
-  # Compare output results
   expect_failure(
-    expect_identical(
-      env1 %>% simmer::get_mon_arrivals(),
-      env2 %>% simmer::get_mon_arrivals()
-    )
-  )
-  expect_failure(
-    expect_identical(
-      env1 %>% simmer::get_mon_resources(),
-      env2 %>% simmer::get_mon_resources()
-    )
+    expect_identical(process_replications(env1), process_replications(env2))
   )
 
   # Repeat experiment, but with multiple replications
   envs1 <- trial(param = defaults())
   envs2 <- trial(param = defaults())
-
-  # Aggregate results from replications in each trial, then compare full trial
-  expect_identical(
-    do.call(rbind, lapply(envs1, simmer::get_mon_arrivals)),
-    do.call(rbind, lapply(envs2, simmer::get_mon_arrivals))
-  )
-  expect_identical(
-    do.call(rbind, lapply(envs1, simmer::get_mon_resources)),
-    do.call(rbind, lapply(envs2, simmer::get_mon_resources))
-  )
+  expect_identical(process_replications(envs1), process_replications(envs2))
 })
